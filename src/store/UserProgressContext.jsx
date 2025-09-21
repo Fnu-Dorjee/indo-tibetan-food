c1f6587908414fc7084180f0436dc5e593cf6e5c
Namgyal-Dorjee <namgyal1455@gmail.com>
2025-09-21 15:04:35 -0400

    Updated

--- src/store/UserProgressContext.jsx
+++ src/store/UserProgressContext.jsx
@@ -35,7 +35,7 @@ export default function UserProgConxtProvider({children}){
         showCart,
         hideCart,
         showCheckOut,
-        hideCheckOut
+        hideCheckOut,
     }
 
     return (
